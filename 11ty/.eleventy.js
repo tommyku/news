dd51e6b63260f49077a94814014e443894687f9c
Tommy Ku <zoo1929@outlook.com>
2022-04-08 23:41:22 +0800

    feat: add thumbnail next to img link

--- 11ty/.eleventy.js
+++ 11ty/.eleventy.js
@@ -2,7 +2,7 @@ const Image = require("@11ty/eleventy-img");
 
 async function image(src) {
   const metadata = await Image(src, {
-    widths: [640],
+    widths: [32, 640],
     formats: ["jpeg"],
     urlPath: "/image",
     outputDir: "./_site/image"
@@ -13,15 +13,22 @@ async function image(src) {
 
 async function imageShortcode(src) {
   const metadata = await image(src);
-  const data = metadata.jpeg[0];
+  const data = metadata.jpeg[1];
   return `<img class="u-photo" src="${data.url}" width="${data.width}" height="${data.height}" loading="lazy" decoding="async" />`;
 }
 
 async function imageLinkShortcode(src) {
   const metadata = await image(src);
-  const data = metadata.jpeg[0];
+  const data = metadata.jpeg[1];
   return `<a class="u-photo" href="${data.url}" target="_blank">${data.filename}</a>`;
 }
+
+async function imageThumbShortcode(src) {
+  const metadata = await image(src);
+  const data = metadata.jpeg[0];
+  return `<img src="${data.url}" width="${data.width}" height="${data.height}" loading="lazy" decoding="async" />`;
+}
+
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
@@ -31,4 +38,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addNunjucksAsyncShortcode("image_link", imageLinkShortcode);
   eleventyConfig.addLiquidShortcode("image_link", imageLinkShortcode);
   eleventyConfig.addJavaScriptFunction("image_link", imageLinkShortcode);
+
+  eleventyConfig.addNunjucksAsyncShortcode("image_thumb", imageThumbShortcode);
+  eleventyConfig.addLiquidShortcode("image_thumb", imageThumbShortcode);
+  eleventyConfig.addJavaScriptFunction("image_thumb", imageThumbShortcode);
 };
